96e46375ce19b88897d45edb7b75c91a5dfc2826
Nicholas <spammiehere@gmail.com>
2023-08-26 12:50:55 -0400

    Add testing

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it } from 'vitest';
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('Renders app', () => {
+    // ARRANGE
+    render(<App />);
+    // ACT
+    // EXPECT
+  });
+});
